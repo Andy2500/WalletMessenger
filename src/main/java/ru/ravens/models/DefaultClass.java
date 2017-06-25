92428e57063d37885f86226fa13fb1f8f7f44d15
andrey <andreynagaev@mail.ru>
2017-06-25 15:45:50 +0300

    DefaultClass added

---
+++ src/main/java/ru/ravens/models/DefaultClass.java
@@ -0,0 +1,35 @@
+package ru.ravens.models;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+@XmlRootElement
+public class DefaultClass implements Serializable {
+
+    private Boolean operationOutput;
+    private String token;
+
+    public DefaultClass() {
+    }
+
+    public DefaultClass(Boolean operationOutput, String token) {
+        this.operationOutput = operationOutput;
+        this.token = token;
+    }
+
+    public Boolean getOperationOutput() {
+        return operationOutput;
+    }
+
+    public void setOperationOutput(Boolean operationOutput) {
+        this.operationOutput = operationOutput;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
