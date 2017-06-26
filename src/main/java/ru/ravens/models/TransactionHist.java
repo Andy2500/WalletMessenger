88ff7d0ac9168987d50733197061ebb7aa2148c8
Alex <aiplesovskikh@edu.hse.ru>
2017-06-26 11:49:27 +0300

    Models

--- src/main/java/ru/ravens/models/TransactionHist.java
+++ src/main/java/ru/ravens/models/TransactionHist.java
@@ -55,6 +55,7 @@ public class TransactionHist implements Serializable
         return transactionHist;
     }
 
+
     public ArrayList<Transaction> getTransactions() {
         return transactions;
     }
